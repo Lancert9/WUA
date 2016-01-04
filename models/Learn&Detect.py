199319188e7e3d8fbd887d71d2badd62d981fb68
Lancert9 <ljwemls@gmail.com>
2016-01-04 11:02:05 +0800

    Update the main function

--- models/Learn&Detect.py
+++ models/Learn&Detect.py
@@ -36,21 +36,28 @@ def main():
 
         with open(url_address, 'rb') as infile:
             record_num = 0
+            study_record_num = 0
             for line in infile:
-                line = line.strip(' \n')
-                record = line.split('\t')
+                record = line.strip(' \n').split('\t')
                 if len(record) == 13 and record[_host] != '':
                     a_url = UrlRecord(record)
                     current_model = host_collector.getHostModel(a_url.getHost())
                     pattern_flag = current_model.getDetectFlag()
+                    # Judge Host-model's pattern('Study ready' or 'Study...')
                     if pattern_flag == 'Study ready':
                         if detect_time_flag:
                             detect_time_flag = False
                             study_ready_time = datetime.datetime.now()
                             study_interval = study_ready_time - begin_time
+                            study_record_num = record_num
                             print 'Study consuming: %s' % study_interval
-                            print 'Study Records: ', record_num
+                            print 'Study Record: %s' % study_record_num
+                            print '\tUrl amount: %s\tDifferent url amount: %s\tDifferent sip: %s\t' % \
+                                (current_model.getUrlAmount(), current_model.getDifUrlAmount(),
+                                 current_model.getSipAmount())
+                        # Detect whether the record is anomaly
                         anomaly_status = anomaly_detector.detect(a_url, current_model)
+                        # If the record is detected to be anomaly
                         if anomaly_status['Result'] is True:
                             valid_flag = result_controller.isValid(a_url, anomaly_collector)
                             if valid_flag is True:
@@ -64,20 +71,21 @@ def main():
                 record_num += 1
                 if record_num % 10000 == 0:
                     print 'Record completed: ', record_num,
-                    print '\tUrl amount: %s\tDifferent url amount: %s\tDifferent sip: %s\t' % (current_model.getUrlAmount(), current_model.getDifUrlAmount(), current_model.getSipAmount())
+            else:
+                detect_record_num = record_num - study_record_num
+                end_time = datetime.datetime.now()
+                detect_ready_time = end_time - study_ready_time
+                print 'Detect Record: %s' % detect_record_num
+                print "Detection consuming: %s" % detect_ready_time
+                print 'Whole system test finished.'
 
+        # Store the completed Host Model.
         with open(host_collector_address, 'wb') as save_host_collector:
             pickle.dump(host_collector, save_host_collector)
 
     except IndexError:
-        print 'Lancer says:\n' \
-           '\tIt has error through the system.'
+        print 'Lancer says:\n\tIt has error through the system.'
         raise
-
-    end_time = datetime.datetime.now()
-    detect_ready_time = end_time - study_ready_time
-    print "Detection consuming: %s" % detect_ready_time
-    print 'Whole system test finished.'
 
 if __name__ == '__main__':
     main()
